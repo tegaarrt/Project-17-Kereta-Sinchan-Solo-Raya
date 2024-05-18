9f3bf4e3d8b99344b8b24b65ed6e9f753fe082a4
Syahla Ahadesi <syahlaahadesi@student.uns.ac.id>
2024-05-18 11:40:21 +0700

    push gambar

--- Progress/progress_program.py
+++ Progress/progress_program.py
@@ -699,7 +699,7 @@ def kelas():
 def pembayaran():
     print("Pembayaran")
 
-image_path = r'Project-17-Kereta-Sinchan-Solo-Raya\kereta sinchan.png'
+image_path = r'Kereta Sinchan Home Page.png'
 original_image = Image.open(image_path)
 resized_image = original_image.resize((1200, 600), Image.Resampling.LANCZOS)
 im = ImageTk.PhotoImage(resized_image)
